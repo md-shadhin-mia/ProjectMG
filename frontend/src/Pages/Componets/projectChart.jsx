475e018613d076b33ec010d7ee92aefb0e9be432
md-shadhin-mia <md-shadhin-mia@users.noreply.github.com>
2023-12-27 14:21:20 +0600

    update projectChart.jsx

--- frontend/src/Pages/Componets/projectChart.jsx
+++ frontend/src/Pages/Componets/projectChart.jsx
@@ -1,93 +1,79 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Line } from 'react-chartjs-2';
-import Chart from 'chart.js/auto';
-import dummeydata from '../projectData';
-
-
+import fetcher from '../../fetcher';
 
 const ProjectChart = () => {
-    const projectData = dummeydata();
-    const chartRef = useRef(null);
-      const taskDates = projectData.goals.map(goal => goal.tasks.map(task => {
-        if (task.completed && task.completed_time) {
-          return {
-            date: new Date(task.completed_time),
-            deadline: new Date(task.deadline)
-          }
-        } else {
-          return {
-            date: new Date(),
-            deadline: new Date(task.deadline)
-          }
-        }
-      }));
-  
-      const goalDates = projectData.goals.map(goal => {
-        const completedTasks = goal.tasks.filter(task => task.completed);
-        if (completedTasks.length === goal.tasks.length) {
-          return {
-            date: new Date(Math.max(...completedTasks.map(task => new Date(task.completed_time)))),
-            deadline: new Date(goal.deadline)
-          }
-        } else {
-          return {
-            date: new Date(),
-            deadline: new Date(goal.deadline)
-          }
-        }
-      });
-  
-      const taskLabels = taskDates.flat().map(d => d.date.toLocaleDateString());
-      const taskData = taskDates.flat().map(d => (d.date - d.deadline) / (1000 * 60 * 60));
-  
-      const goalLabels = goalDates.map(d => d.date.toLocaleDateString());
-      const goalData = goalDates.map(d => (d.date - d.deadline) / (1000 * 60 * 60));
-  
-       let  garaph ={
-        type: 'line',
-        data: {
-            labels: taskLabels,
-            datasets: [
-                {
-                    label: 'Task Completion Duration',
-                    data: taskData,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.6)'
-                    ],
-                    borderWidth: 4
-                },
-                {
-                    label: 'Goal Completion Duration',
-                    data: goalData,
-                    backgroundColor: [
-                        'rgba(54, 162, 235, 0.6)'
-                    ],
-                    borderWidth: 4
-                    }
-                ]
-        },
-        options: {
-                responsive: true,
-                title: {
-                    text: 'Task and Goal Completion Duration',
-                    display: true
-                },
-                scales: {
-                    y: {
-                        label: 'Duration (hours)',
-                        beginAtZero: true
-                    }
+    const [report, setReport] = useState(null);
+
+    useEffect(() => {
+        fetcher.getReportSummary().then(res => setReport(res.data)).catch(() => {});
+    }, []);
+
+    const labels = report ? report.projects.map(p => p.title) : [];
+    const taskData = report ? report.projects.map(p => p.taskCount) : [];
+    const completedData = report ? report.projects.map(p => p.completedTasks) : [];
+
+    const chartData = {
+        labels: labels,
+        datasets: [
+            {
+                label: 'Total Tasks',
+                data: taskData,
+                backgroundColor: 'rgba(99, 102, 241, 0.1)',
+                borderColor: 'rgba(99, 102, 241, 0.5)',
+                borderWidth: 2,
+                pointRadius: 3,
+                pointBackgroundColor: 'rgba(99, 102, 241, 0.8)',
+                tension: 0.3
+            },
+            {
+                label: 'Completed Tasks',
+                data: completedData,
+                backgroundColor: 'rgba(14, 165, 233, 0.1)',
+                borderColor: 'rgba(14, 165, 233, 0.5)',
+                borderWidth: 2,
+                pointRadius: 3,
+                pointBackgroundColor: 'rgba(14, 165, 233, 0.8)',
+                tension: 0.3
+            }
+        ]
+    };
+
+    const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                labels: {
+                    boxWidth: 8,
+                    boxHeight: 8,
+                    usePointStyle: true,
+                    font: { size: 11, family: 'Inter' }
                 }
             }
+        },
+        scales: {
+            x: {
+                grid: { display: false },
+                ticks: { font: { size: 10, family: 'Inter' } }
+            },
+            y: {
+                beginAtZero: true,
+                grid: { color: 'rgba(0,0,0,0.04)' },
+                ticks: { font: { size: 10, family: 'Inter' } }
+            }
         }
-
+    };
 
     return (
-      <div className="flex-shrink max-w-full pr-2 w-full lg:w-1/2 mb-6">
-        <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg p-6 h-full">
-            <Line data={garaph.data} ref={chartRef} options={garaph.options}/>
+        <div className="card h-full">
+            <div className="mb-4">
+                <h4 className="text-xs font-medium text-gray-400 uppercase tracking-wider">Analytics</h4>
+            </div>
+            <div className="h-64">
+                {report ? <Line data={chartData} options={options} /> : <div className="flex items-center justify-center h-full text-gray-400 text-sm">Loading...</div>}
+            </div>
         </div>
-      </div>
     );
 };
 
