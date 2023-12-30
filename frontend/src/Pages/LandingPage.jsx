de63bfb10e0516f7f88b3cef4579aa832d5f4b07
md-shadhin-mia <md-shadhin-mia@users.noreply.github.com>
2023-12-30 20:31:15 +0600

    feat: redesign landing page with hero, features, testimonials

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -1,52 +1,319 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+const features = [
+  {
+    title: 'Project Management',
+    description: 'Organize projects with milestones, deadlines, and team assignments in one place.',
+    icon: (
+      <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
+      </svg>
+    ),
+  },
+  {
+    title: 'Task Tracking',
+    description: 'Break projects into tasks, assign priorities, track progress with drag-and-drop boards.',
+    icon: (
+      <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
+      </svg>
+    ),
+  },
+  {
+    title: 'Goal Setting',
+    description: 'Define measurable goals and link them to tasks for clear direction.',
+    icon: (
+      <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M13 10V3L4 14h7v7l9-11h-7z" />
+      </svg>
+    ),
+  },
+  {
+    title: 'Team Collaboration',
+    description: 'Invite team members, assign roles, and collaborate in real-time.',
+    icon: (
+      <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+      </svg>
+    ),
+  },
+  {
+    title: 'Dashboard & Reports',
+    description: 'Visualize progress with charts, burndown graphs, and custom reports.',
+    icon: (
+      <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+      </svg>
+    ),
+  },
+  {
+    title: 'Secure & Fast',
+    description: 'JWT authentication, encrypted data, and blazing fast performance.',
+    icon: (
+      <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+      </svg>
+    ),
+  },
+];
+
+const stats = [
+  { value: '10K+', label: 'Projects Managed' },
+  { value: '50K+', label: 'Tasks Completed' },
+  { value: '99.9%', label: 'Uptime' },
+  { value: '4.9/5', label: 'User Rating' },
+];
+
+const testimonials = [
+  {
+    name: 'Sarah Chen',
+    role: 'Engineering Lead at TechCorp',
+    text: 'ProjectMG transformed how our team handles sprints. We ship 40% faster now.',
+    avatar: 'SC',
+  },
+  {
+    name: 'Marcus Rodriguez',
+    role: 'CTO at StartupXYZ',
+    text: 'The clean interface and powerful features make this our go-to project tool.',
+    avatar: 'MR',
+  },
+  {
+    name: 'Aisha Patel',
+    role: 'Product Manager at ScaleUp',
+    text: 'Finally a tool that does everything without the bloat. Love the dashboard.',
+    avatar: 'AP',
+  },
+];
 
 const LandingPage = () => {
-    return (
-        <div className="flex flex-col items-center">
-            {/* Banner */}
-            <div className="bg-indigo-500 p-10 text-center text-white">
-                <h1 className="text-3xl font-bold">Programming Project Management System</h1>
-                <p className="text-lg">Efficiently manage your programming projects with ease</p>
+  return (
+    <div className="min-h-screen bg-white">
+      {/* Navigation */}
+      <nav className="fixed w-full top-0 z-50 bg-white/80 backdrop-blur-md border-b border-gray-100">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center gap-2">
+              <div className="w-8 h-8 bg-indigo-600 rounded-lg flex items-center justify-center">
+                <svg className="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 10V3L4 14h7v7l9-11h-7z" />
+                </svg>
+              </div>
+              <span className="text-xl font-bold text-gray-900">ProjectMG</span>
             </div>
-
-            {/* Features */}
-            <div className="mt-10 px-10">
-                <h2 className="text-2xl font-bold mb-5">Features</h2>
-                <ul className="list-disc">
-                    <li className="mb-2">Project management</li>
-                    <li className="mb-2">Version control</li>
-                    <li className="mb-2">Task management</li>
-                    <li className="mb-2">Issue tracking</li>
-                    <li className="mb-2">Communication</li>
-                    <li className="mb-2">Reporting</li>
-                    <li className="mb-2">Code review</li>
-                    <li className="mb-2">Continuous integration and deployment</li>
-                </ul>
+            <div className="flex items-center gap-4">
+              <Link to="/login" className="text-gray-600 hover:text-gray-900 font-medium text-sm transition-colors">
+                Sign in
+              </Link>
+              <Link
+                to="/register"
+                className="bg-indigo-600 text-white px-4 py-2 rounded-lg text-sm font-medium hover:bg-indigo-700 transition-colors"
+              >
+                Get Started Free
+              </Link>
             </div>
-
-            {/* About */}
-            <div className="mt-10 px-10">
-                <h2 className="text-2xl font-bold mb-5">About</h2>
-                <p className="text-lg mb-5">
-                    The Programming Project Management System is a web-based application that helps developers and project managers to plan, execute, and track software development projects.
-                </p>
-            </div>
-
-            {/* Reviews */}
-            <div className="mt-10 px-10">
-                <h2 className="text-2xl font-bold mb-5">Reviews</h2>
-                <p className="text-lg mb-5">
-                    "The Programming Project Management System has been a game-changer for our team. It has improved our workflow and made it easier for us to track the progress of our projects." - John Doe
-                </p>
-            </div>
-
-            {/* Pricing */}
-            <div className="mt-10 px-10">
-                <h2 className="text-2xl font-bold mb-5">Pricing</h2>
-                <p className="text-lg mb-5">Starting at $10/month</p>
-            </div>
+          </div>
         </div>
-    );
+      </nav>
+
+      {/* Hero */}
+      <section className="pt-32 pb-20 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-4xl mx-auto text-center">
+          <div className="inline-flex items-center gap-2 bg-indigo-50 text-indigo-700 px-4 py-1.5 rounded-full text-sm font-medium mb-8">
+            <span className="w-2 h-2 bg-indigo-500 rounded-full animate-pulse"></span>
+            Now in Open Beta
+          </div>
+          <h1 className="text-5xl sm:text-6xl font-bold text-gray-900 leading-tight mb-6">
+            Ship Projects
+            <span className="text-indigo-600"> Faster</span>
+            <br />
+            With Your Team
+          </h1>
+          <p className="text-xl text-gray-500 max-w-2xl mx-auto mb-10 leading-relaxed">
+            The modern project management platform built for development teams.
+            Track tasks, set goals, and deliver on time — all in one place.
+          </p>
+          <div className="flex flex-col sm:flex-row items-center justify-center gap-4">
+            <Link
+              to="/register"
+              className="w-full sm:w-auto bg-indigo-600 text-white px-8 py-3.5 rounded-xl text-base font-semibold hover:bg-indigo-700 transition-all shadow-lg shadow-indigo-200 hover:shadow-xl hover:shadow-indigo-200"
+            >
+              Start for Free
+            </Link>
+            <a
+              href="#features"
+              className="w-full sm:w-auto flex items-center justify-center gap-2 text-gray-600 px-8 py-3.5 rounded-xl text-base font-semibold border border-gray-200 hover:border-gray-300 hover:bg-gray-50 transition-all"
+            >
+              <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+              See How It Works
+            </a>
+          </div>
+        </div>
+
+        {/* Hero Image / Preview */}
+        <div className="max-w-5xl mx-auto mt-16 px-4">
+          <div className="bg-gray-900 rounded-2xl p-1 shadow-2xl shadow-gray-300">
+            <div className="bg-gray-800 rounded-xl overflow-hidden">
+              <div className="flex items-center gap-2 px-4 py-3 bg-gray-800">
+                <div className="flex gap-1.5">
+                  <div className="w-3 h-3 rounded-full bg-red-500"></div>
+                  <div className="w-3 h-3 rounded-full bg-yellow-500"></div>
+                  <div className="w-3 h-3 rounded-full bg-green-500"></div>
+                </div>
+                <div className="flex-1 text-center text-gray-400 text-sm">ProjectMG Dashboard</div>
+              </div>
+              <div className="bg-white p-6 grid grid-cols-3 gap-4">
+                <div className="col-span-1 space-y-3">
+                  <div className="h-4 bg-indigo-100 rounded w-3/4"></div>
+                  <div className="h-3 bg-gray-100 rounded w-full"></div>
+                  <div className="h-3 bg-gray-100 rounded w-5/6"></div>
+                  <div className="h-3 bg-gray-100 rounded w-2/3"></div>
+                  <div className="mt-4 h-24 bg-indigo-50 rounded-lg"></div>
+                </div>
+                <div className="col-span-2 space-y-3">
+                  <div className="grid grid-cols-3 gap-3">
+                    <div className="h-20 bg-green-50 rounded-lg border border-green-100"></div>
+                    <div className="h-20 bg-blue-50 rounded-lg border border-blue-100"></div>
+                    <div className="h-20 bg-amber-50 rounded-lg border border-amber-100"></div>
+                  </div>
+                  <div className="h-40 bg-gray-50 rounded-lg border border-gray-100 flex items-center justify-center">
+                    <svg className="w-12 h-12 text-gray-300" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Stats */}
+      <section className="py-16 bg-gray-50 border-y border-gray-100">
+        <div className="max-w-5xl mx-auto px-4 grid grid-cols-2 md:grid-cols-4 gap-8">
+          {stats.map((stat) => (
+            <div key={stat.label} className="text-center">
+              <div className="text-3xl sm:text-4xl font-bold text-indigo-600">{stat.value}</div>
+              <div className="text-sm text-gray-500 mt-1">{stat.label}</div>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      {/* Features */}
+      <section id="features" className="py-24 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-6xl mx-auto">
+          <div className="text-center mb-16">
+            <h2 className="text-3xl sm:text-4xl font-bold text-gray-900 mb-4">
+              Everything You Need
+            </h2>
+            <p className="text-lg text-gray-500 max-w-2xl mx-auto">
+              A complete toolkit for managing software projects from idea to deployment.
+            </p>
+          </div>
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {features.map((feature) => (
+              <div
+                key={feature.title}
+                className="group p-6 rounded-2xl border border-gray-100 hover:border-indigo-100 hover:shadow-lg hover:shadow-indigo-50 transition-all duration-300"
+              >
+                <div className="w-12 h-12 bg-indigo-50 text-indigo-600 rounded-xl flex items-center justify-center mb-4 group-hover:bg-indigo-600 group-hover:text-white transition-colors duration-300">
+                  {feature.icon}
+                </div>
+                <h3 className="text-lg font-semibold text-gray-900 mb-2">{feature.title}</h3>
+                <p className="text-gray-500 leading-relaxed">{feature.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      {/* Testimonials */}
+      <section className="py-24 bg-gray-50 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-6xl mx-auto">
+          <div className="text-center mb-16">
+            <h2 className="text-3xl sm:text-4xl font-bold text-gray-900 mb-4">
+              Loved by Teams
+            </h2>
+            <p className="text-lg text-gray-500">
+              See what project managers and developers are saying.
+            </p>
+          </div>
+          <div className="grid md:grid-cols-3 gap-8">
+            {testimonials.map((t) => (
+              <div key={t.name} className="bg-white p-6 rounded-2xl shadow-sm border border-gray-100">
+                <div className="flex items-center gap-1 mb-4">
+                  {[...Array(5)].map((_, i) => (
+                    <svg key={i} className="w-5 h-5 text-amber-400 fill-current" viewBox="0 0 20 20">
+                      <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
+                    </svg>
+                  ))}
+                </div>
+                <p className="text-gray-600 mb-6 leading-relaxed">"{t.text}"</p>
+                <div className="flex items-center gap-3">
+                  <div className="w-10 h-10 bg-indigo-100 text-indigo-700 rounded-full flex items-center justify-center font-semibold text-sm">
+                    {t.avatar}
+                  </div>
+                  <div>
+                    <div className="font-medium text-gray-900 text-sm">{t.name}</div>
+                    <div className="text-gray-400 text-xs">{t.role}</div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      {/* CTA */}
+      <section className="py-24 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-3xl mx-auto text-center">
+          <h2 className="text-3xl sm:text-4xl font-bold text-gray-900 mb-4">
+            Ready to Get Started?
+          </h2>
+          <p className="text-lg text-gray-500 mb-8">
+            Join thousands of teams shipping better software with ProjectMG.
+            Free for up to 5 projects.
+          </p>
+          <Link
+            to="/register"
+            className="inline-flex items-center gap-2 bg-indigo-600 text-white px-8 py-4 rounded-xl text-base font-semibold hover:bg-indigo-700 transition-all shadow-lg shadow-indigo-200 hover:shadow-xl"
+          >
+            Create Your Free Account
+            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 7l5 5m0 0l-5 5m5-5H6" />
+            </svg>
+          </Link>
+        </div>
+      </section>
+
+      {/* Footer */}
+      <footer className="border-t border-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between gap-6">
+          <div className="flex items-center gap-2">
+            <div className="w-7 h-7 bg-indigo-600 rounded-lg flex items-center justify-center">
+              <svg className="w-4 h-4 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 10V3L4 14h7v7l9-11h-7z" />
+              </svg>
+            </div>
+            <span className="font-semibold text-gray-900">ProjectMG</span>
+          </div>
+          <div className="flex items-center gap-6 text-sm text-gray-400">
+            <a href="#features" className="hover:text-gray-600 transition-colors">Features</a>
+            <span>Privacy</span>
+            <span>Terms</span>
+            <span>Docs</span>
+          </div>
+          <div className="text-sm text-gray-400">
+            Built with Spring Boot + React
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default LandingPage;
