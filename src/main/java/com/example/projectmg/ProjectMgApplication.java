7bcf03176d644ffba5cf2b16a25477aa40b9499a
md-shadhin-mia <md-shadhin-mia@users.noreply.github.com>
2024-01-10 15:50:29 +0600

    fix: add EntityScan and EnableJpaRepositories

--- src/main/java/com/example/projectmg/ProjectMgApplication.java
+++ src/main/java/com/example/projectmg/ProjectMgApplication.java
@@ -2,8 +2,12 @@ package com.example.projectmg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.example.projectmg.JPA", "com.example.projectmg.goal"})
+@EnableJpaRepositories(basePackages = {"com.example.projectmg.JPA", "com.example.projectmg.goal"})
 public class ProjectMgApplication {
 
     public static void main(String[] args) {
